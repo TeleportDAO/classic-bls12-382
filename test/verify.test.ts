c78f8bad2f8fcaeb7bb8748f1172030c92aa09db
CrazyMhd <mahdi@teleportdao.xyz>
2023-05-05 17:29:55 +0300

    One pairing test

--- test/verify.test.ts
+++ test/verify.test.ts
@@ -13,6 +13,97 @@ const g2AddTestVector = require("./fixtures/g2_add.json")
 
 describe("Verification", () => {
 
+    it.only("check pairing tests", function() {
+
+        let P = new point (
+            fp1FromBigInt(0x00000000000000000000000000000000043c4ff154778330b4d5457b7811b551dbbf9701b402230411c527282fb5d2ba12cb445709718d5999e79fdd74c0a670n),
+            fp1FromBigInt(0x00000000000000000000000000000000013a80ede40df002b72f6b33b1f0e3862d505efbe0721dce495d18920d542c98cdd2daf5164dbd1a2fee917ba943deben),
+            false 
+        )
+    
+        let Q = new point (
+            new Fp2(
+                fp1FromBigInt(0x0000000000000000000000000000000001c2d8d353d5983f22a5313ddd58fdc0d9c994b2915dbc87a9b65b7b98ff00b62e140a27dc322d42b3ad190c1b3728ddn),
+                fp1FromBigInt(0x0000000000000000000000000000000010412f3625947b38bb380a6ed059f1677b7a7afcb91517837c563dadd0e285b95740a200ddff6570d4d92bb636b625bbn),
+            ),
+            new Fp2(
+                fp1FromBigInt(0x0000000000000000000000000000000015f4f9a480a57bd1b2388532ab045a1ba93d2f6589a3022c585fe06a1d611165c99d70be06251812405c9c37d6e9f773n),
+                fp1FromBigInt(0x0000000000000000000000000000000001a78e6c5062a6634a56e9853ff5afacb2e7cf31fd0ea5f0d8c8ac6174c88133cf2f63450ec4590544c9a0e37daac1f9n),
+            ),
+            false
+        )
+    
+        expect(
+            P.isOnCurve()
+        ).to.equal(true)
+
+        expect(
+            Q.isOnCurve()
+        ).to.equal(true)
+    
+        expect(
+            P.isInSubGroup()
+        ).to.equal(true)
+
+        expect(
+            Q.isInSubGroup()
+        ).to.equal(true)
+
+
+        let R = new point (
+            fp1FromBigInt(0x00000000000000000000000000000000043c4ff154778330b4d5457b7811b551dbbf9701b402230411c527282fb5d2ba12cb445709718d5999e79fdd74c0a670n),
+            fp1FromBigInt(0x0000000000000000000000000000000018c690fc5571f69793ec3c82915ac9513726ec891312f4f11dd3ba0ee95cc98b50d925099b0642e58a106e8456bbcbedn),
+            false 
+        )
+    
+        let S = new point (
+            new Fp2(
+                fp1FromBigInt(0x0000000000000000000000000000000001c2d8d353d5983f22a5313ddd58fdc0d9c994b2915dbc87a9b65b7b98ff00b62e140a27dc322d42b3ad190c1b3728ddn),
+                fp1FromBigInt(0x0000000000000000000000000000000010412f3625947b38bb380a6ed059f1677b7a7afcb91517837c563dadd0e285b95740a200ddff6570d4d92bb636b625bbn),
+            ),
+            new Fp2(
+                fp1FromBigInt(0x0000000000000000000000000000000015f4f9a480a57bd1b2388532ab045a1ba93d2f6589a3022c585fe06a1d611165c99d70be06251812405c9c37d6e9f773n),
+                fp1FromBigInt(0x0000000000000000000000000000000001a78e6c5062a6634a56e9853ff5afacb2e7cf31fd0ea5f0d8c8ac6174c88133cf2f63450ec4590544c9a0e37daac1f9n),
+            ),
+            false
+        )
+
+        expect(
+            R.isOnCurve()
+        ).to.equal(true)
+
+        expect(
+            S.isOnCurve()
+        ).to.equal(true)
+    
+        expect(
+            R.isInSubGroup()
+        ).to.equal(true)
+
+        expect(
+            S.isInSubGroup()
+        ).to.equal(true)
+        
+
+        let pairingRes = pairing(P, Q)
+        // let pairingRes = pairing(P, Hm)
+        console.log("pairingRes1: ")
+        pairingRes.displayInfo()
+
+        let pairingRes2 = pairing(R, S)
+        console.log("pairingRes2: ")
+        pairingRes2.displayInfo()
+    
+        // expect(
+        //     pairingRes.mul(pairingRes2).equalOne()
+        // ).to.equal(true)
+
+        expect(
+            pairingRes.mul(pairingRes2).equalOne()
+        ).to.equal(true)
+
+    })
+
     it("verify", function() {
         let P = new point (
             fp1FromBigInt(3071902358779104425805220059913391042958977442368743450008922736970201383908820407429457646333339330346464018568299n),
